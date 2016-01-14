6bce6cd7cdedd5c65d3e3015dc946cbc36fd2ce2
Ingo Fabbri <i.fabbri@newtown.at>
2016-01-14 14:44:16 +0100

    escape jQuery-selectors containing colons

--- skin/frontend/base/default/js/quafzi_fixedbillingaddress/use_billing_for_shipping_forbidden.js
+++ skin/frontend/base/default/js/quafzi_fixedbillingaddress/use_billing_for_shipping_forbidden.js
@@ -1,11 +1,15 @@
 jQuery(document).ready(function () {
     //Billing step
-    jQuery('#billing:use_for_shipping_yes').prop('checked', false)(); //uncheck "Ship to this address"
-    jQuery('#billing:use_for_shipping_yes').hide();
-    jQuery('#billing:use_for_shipping_no').prop('checked', true)(); //check "Ship to different address"
-    jQuery('#billing:use_for_shipping_no').hide();
+    jQuery('#billing\\:use_for_shipping_yes').prop('checked', false); //uncheck "Ship to this address"
+    jQuery('#billing\\:use_for_shipping_yes').hide(); //hide radio
+    jQuery("label[for='billing\\:use_for_shipping_yes']").remove(); //remove the according label element
+
+    jQuery('#billing\\:use_for_shipping_no').prop('checked', true); //check "Ship to different address"
+    jQuery('#billing\\:use_for_shipping_no').hide(); //hide radio
+    jQuery("label[for='billing\\:use_for_shipping_no']").remove(); //remove the according label element
 
     //Shipping step
-    jQuery('#shipping:same_as_billing').prop('checked', false)(); //uncheck "Use billing address"
-    jQuery('#shipping:same_as_billing').hide();
+    jQuery('#shipping\\:same_as_billing').prop('checked', false); //uncheck "Use billing address"
+    jQuery('#shipping\\:same_as_billing').hide(); //hide radio
+    jQuery("label[for='shipping\\:same_as_billing']").remove(); //remove the according label element
 });
